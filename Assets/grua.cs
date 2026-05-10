55771474e3cd90b2e1236ad8554f6cf35ae168ba
paupanfe <paupanfe@alumni.uv.es>
2026-05-10 19:27:11 +0200

    uwu

--- Assets/grua.cs
+++ Assets/grua.cs
@@ -2,90 +2,89 @@ using UnityEngine;
 
 public class grua : MonoBehaviour
 {
-   
-    public float speed = 5f;
+    public Material materialCuerda;
+    public Transform cuerda;
+    public Transform objeto;
+    public Transform pivoteGrua;
+    public Transform BaseGrua;
+
+    public float velGrua = 10f;
+    public float vel = 10f;
     public float minZ = 0.75f;
     public float maxZ = 7.25f;
-    private float posicionZ = 0f;
+    private float posZ = 0f;
 
-   
-    public float velocidadGancho = 10f;
-
-    public Transform cuerda;
-    public Transform objetoCuelga;
-    public Transform pivoteGancho;
-    public Transform puntoAnclajeSuperior;
-    public Material materialCuerda;
+    
 
     private Vector3 escalaOriginal;
     private float distanciaInicial;
 
-    void Start()
+   void Start()
     {
-        posicionZ = transform.position.z;
+        posZ = transform.position.z;
 
         if (cuerda != null)
         {
             escalaOriginal = cuerda.localScale;
         }
 
-        if (puntoAnclajeSuperior == null)
+        if (cuerda != null && BaseGrua != null && objeto != null)
         {
-            puntoAnclajeSuperior = this.transform;
-        }
-
-        if (cuerda != null && puntoAnclajeSuperior != null && objetoCuelga != null)
-        {
-            Vector3 dirInicial = objetoCuelga.position - puntoAnclajeSuperior.position;
+            Vector3 dirInicial = objeto.position - BaseGrua.position;
             distanciaInicial = dirInicial.magnitude;
         }
     }
 
     void Update()
     {
+
         // Movimiento de la grua
-        float movimientoZ = speed * Time.deltaTime;
+        float movimientoZ = vel * Time.deltaTime;
         Vector3 pos = transform.position;
 
         if (Input.GetKey(KeyCode.O) && pos.z + movimientoZ <= maxZ)
         {
             transform.Translate(Vector3.forward * movimientoZ, Space.World);
-            posicionZ += movimientoZ;
+            posZ += movimientoZ;
         }
-        else if (Input.GetKey(KeyCode.P) && pos.z - movimientoZ >= minZ)
+        else 
         {
-            transform.Translate(Vector3.back * movimientoZ, Space.World);
-            posicionZ -= movimientoZ;
+            if (Input.GetKey(KeyCode.L) && pos.z - movimientoZ >= minZ)
+            {
+                transform.Translate(Vector3.back * movimientoZ, Space.World);
+                posZ -= movimientoZ;
+            }
+                
         }
 
         // subida y bajada
-        if (pivoteGancho != null)
+        if (pivoteGrua != null)
         {
-            Vector3 posPivote = pivoteGancho.localPosition;
-            float movimiento = velocidadGancho * Time.deltaTime;
+            Vector3 posPivote = pivoteGrua.localPosition;
+            float movimiento = velGrua * Time.deltaTime;
             float nuevaY = posPivote.y;
 
             // Subir
-            if (Input.GetKey(KeyCode.K))
+            if (Input.GetKey(KeyCode.I))
             {
                 nuevaY -= movimiento;
             }
 
             // Bajar
-            if (Input.GetKey(KeyCode.L))
+            if (Input.GetKey(KeyCode.K))
             {
                 nuevaY += movimiento;
             }
 
             // Limitar
             nuevaY = Mathf.Clamp(nuevaY, -80f, 0f);
-            pivoteGancho.localPosition = new Vector3(posPivote.x, nuevaY, posPivote.z);
+            pivoteGrua.localPosition = new Vector3(posPivote.x, nuevaY, posPivote.z);
         }
 
         // posiciones
-        if (objetoCuelga != null && pivoteGancho != null)
+        if (objeto != null && pivoteGrua != null)
         {
-            objetoCuelga.position = pivoteGancho.position;
+            objeto.position = pivoteGrua.position;
         }
 
         ActualizarCuerda();
@@ -93,27 +92,43 @@ public class grua : MonoBehaviour
 
     private void ActualizarCuerda()
     {
-        if (cuerda == null || objetoCuelga == null || puntoAnclajeSuperior == null) return;
-
-        Vector3 direccion = objetoCuelga.position - puntoAnclajeSuperior.position;
+        bool todoOk = (cuerda != null && objeto != null && BaseGrua != null);
+        float factorEscala;
+        Vector3 direccion = objeto.position - BaseGrua.position;
         float distancia = direccion.magnitude;
 
-        cuerda.position = puntoAnclajeSuperior.position + direccion * 0.5f;
-
-        cuerda.rotation = Quaternion.LookRotation(direccion);
-        cuerda.Rotate(90f, 0f, 0f);
-
-        float factorEscala = (distanciaInicial > 0) ? (distancia / distanciaInicial) : 1f;
-
-        cuerda.localScale = new Vector3(
-            escalaOriginal.x,
-            escalaOriginal.y * factorEscala,
-            escalaOriginal.z
-        );
-
-        if (materialCuerda != null)
+        if (todoOk)
         {
-            materialCuerda.mainTextureScale = new Vector2(1, factorEscala);
+
+            cuerda.position = BaseGrua.position + direccion * 0.5f;
+
+            
+            cuerda.rotation = Quaternion.LookRotation(direccion);
+            cuerda.Rotate(90f, 0f, 0f);
+
+
+
+            if (distanciaInicial > 0)
+            {
+                factorEscala = distancia / distanciaInicial;
+            }
+            else
+            {
+                factorEscala = 1f;
+            }
+
+            cuerda.localScale = new Vector3(
+                escalaOriginal.x,
+                escalaOriginal.y * factorEscala,
+                escalaOriginal.z
+            );
+
+ 
+            if (materialCuerda != null)
+            {
+                materialCuerda.mainTextureScale = new Vector2(1, factorEscala);
+            }
         }
+      
     }
 }
